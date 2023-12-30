551ee5e4a64099e2aa5fd1e0d15c7fdf8b50f22f
camilasukhada <camilasukhada@gmail.com>
2023-12-30 16:17:01 -0300

    testes aprovados

--- src/index.js
+++ src/index.js
@@ -5,13 +5,24 @@ const textarea=document.querySelector("textarea");
 const botao=document.getElementById("reset-button");
 
 function atualizaCaracter(){
-    document.querySelector("li[data-testid='word-count']").textContent="Contagem de palavras: " + analyzer.getWordCount(textarea.value);
+
+  document.querySelector("li[data-testid='word-count']").textContent="Contagem de palavras: " + analyzer.getWordCount(textarea.value);
+
+  document.querySelector("li[data-testid='character-count']").textContent="Contagem de caracteres: " + analyzer.getCharacterCount(textarea.value);
+
+  document.querySelector("li[data-testid='character-no-spaces-count']").textContent="Contagem de caracteres excluindo espaços e sinais de pontuação: "+ analyzer.getCharacterCountExcludingSpaces(textarea.value);
+
+  document.querySelector("li[data-testid='number-count']").textContent="Contagem de números: "+ analyzer.getNumberCount(textarea.value);
+
+  document.querySelector("li[data-testid='number-sum']").textContent="Soma total de números: "+ analyzer.getNumberSum(textarea.value);
+
+  document.querySelector("li[data-testid='word-length-average']").textContent="Comprimento médio das palavras: "+ analyzer. getAverageWordLength(textarea.value) +" caracteres.";
+    
 }
-    textarea.addEventListener('input', atualizaCaracter);
-    botao.addEventListener('click', () =>{
-        textarea.value='';
-        atualizaCaracter();
-
-
+    
+textarea.addEventListener('input', atualizaCaracter);
+botao.addEventListener('click', () =>{
+  textarea.value='';
+  atualizaCaracter();
 });
 
