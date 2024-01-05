4756eeb380579f3235452b1415454e105dd5bb33
camilasukhada <camilasukhada@gmail.com>
2024-01-05 13:01:34 -0300

    Updates

--- analyzer.js
+++ analyzer.js
@@ -2,101 +2,120 @@
     
 const analyzer = { 
   
-  //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.  
+  // Esta função deve retorna a contagem de palavras que se encontram no parâmetro 'text' de tipo 'string':
   getWordCount: (text) => {
-    const words= text.trim().split(/\s+/);
-    return words.length;
+    const words= text.trim().split(' '); 
+    return words.length;  
+
+    // O método "trim" retira os espaços do início e do fim da string.
+    // O  método "split" "fatia" a string em array seguindo o regex
+    // que determina que o breakpoint que separa um elemento da array é o espaço que separa antes e depois,
+    // A propriedade "lenght" contém o comprimento da string, que vai retornar o número de elementos da array, que equivale a métrica
+    // do número de palavras retornada na tela da usuária.
+  
   },
 
-  //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.   
+
+  // Esta função retorna a contagem de caracteres que se encontram no parâmetro 'text' de tipo 'string':
   getCharacterCount: (text) => {
-    return text.length;
+    return text.length;   
+
+    //a propriedade "lenght" contém o comprimento da 'string', que equivale a métrica de número de caracteres retornada na tela da usuária.
+
   },
 
-  //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+
+  // Esta função retorna a contagem de caracteres exluindo espaçoes e pontuações que se encontram no parâmetro 'text' de tipo 'string':
   getCharacterCountExcludingSpaces: (text) => {
     const textExcludingSpaces = text.replace(/[^\w]/g,'');
     const characterCountExcludingSpaces = textExcludingSpaces.length;
     return characterCountExcludingSpaces;
+
+    // O método "replace" retorna uma nova string correspondente ao padrão determinado no regex, que é retirar espaços e pontuações.
+    // A essa nova string é aplicada a propriedade "lenght" que contém o comprimento da string, que equivale a métrica de números de 
+    // caracteres excluindo espaços e pontuações retornada na tela da usuária.
+
   },
    
-  //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+
+  // Esta função retorna quantos números se encontram no parâmetro 'text' de tipo 'string':
   getNumberCount: (text) => {
     const number = text.match(/\b\d+(\.\d+)?\b/g);
-
     if (number) {
       return number.length;
     } else {
       return 0; 
     }
 
+    // O método "match" retorna uma array correspondente ao padrão determinado no regex, que é trazer somente os números presentes da string numa lista.
+    // Se 'number' existe (e ele só axiste se atender as condições do "match"), então a propriedade "lenght" traz o comprimento da string,
+    // que vai retornar o número de elementos da array, que equivale a métrica do número de numerais retornada na tela da usuária.
+
   },
 
-  //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+
+  // Esta função retorna a soma de todos os números que se encontram no parâmetro 'text' do tipo 'string': 
   getNumberSum: (text) => {
     const sumNumber = text.match(/\b\d+(\.\d+)?\b/g);
     if (sumNumber) {
       let result = 0;
-      for (let i = 0; i<sumNumber.length;i++){
-        result+= Number(sumNumber[i]);
-      }
+      sumNumber.forEach((SumOfNumbers)=>{result+=Number(SumOfNumbers)})
+
+      // O método "match" retorna uma array correspondente ao padrão determinado no regex, que é trazer somente os números presentes da string numa lista.
+      // Se 'sumNumber' existe, o metódo "foreach" aplica a cada item da array a soma desses itens em 'result', e retorna a soma dos números na
+      // tela da usuária.
+
       return result;
+
+      // e quando não atender mais as condições do 'foreach'- que atua com um loop:
 
     } else{
       return 0;
     }
+    //-----------------------------------------------------------------------------------------------------------------------
+    //  O método "match" retorna uma array correspondente ao padrão determinado no regex, que é trazer somente os números presentes da string numa lista.
+    // A condicional "if" declara a variável 'result'.
+    // E o loop "for" define que para o "i" menor que o número de elementos da array obtida pelo método "match", soma-se esses elementos.
+    // O objeto "Number" permite converter o resultado dessa soma num valor de tipo 'number'.
+    // Que equivale a métrica da soma dos números, retornada na tela da usuária.
+    // O "else" retorna o que não for verdadeiro à condição do "if".
 
-    // for (let i = 0; i < text.length; i++) {
-    //       if (text[i].match(/\d+/g)) {
-    //         sumOfNumbers += parseInt(text[i], 10);
-
-    // retornar os números de 0 a 9 da textarea, num array com números de 1 digito.
-
-    // const numbersSum = text.match(/\b\d+(\.\d+)?\b/g);
-    // if (numbersSum) {
-    //   let result = 0;
-    //   for (let i = 0; i<numbersSum.length;i++){
-    //     result += Number(numbersSum[i]);
-    //   }
-    //   return result;
-    //   console.log (result);
-    
-    // } else {
-    //   return 0;
-    // }
-
-    //-----------------------------------------------------------------------------------
-    // let sumOfNumber = text.match(/\d+/g);
-    // console.log(sumOfNumber);
-
-    //   for (let i = 0; i < text.length; i++) {
-    //     if (text[i].match(/\d+/g)) {
-    //       sumOfNumber += parseInt(text[i], 10);
-    //     }
-    //   }
-    //---------------------------------------------------------------------------------
-    // const numberSum = text.match(/\d+/g);
-    // var sumOfNumber = 0;
-
-    // if(numberSum) {
-    //   for (var i =0; i < numberSum.lenght; i++) {
-    //     sumOfNumber += parseInt(numberSum[i],10);
-    //   }
+    // estrutura do "for": 
+    //for ( [expressão inicial]; [expressão condicional]; [atualização da expressão inicial] ) {
+    // um ou vários comandos a serem executados //
+    //declaração;
+    //}
 
   },
 
-  //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
-  getAverageWordLength: (text) => {    
-    const avarageWord = text.split(' ');
-    let count = 0;
+  // Esta função retorna o comprimento médio das palavras que se encontram no parâmetro 'text'de tipo 'string':
+  getAverageWordLength: (text) => {  
+    const numberOfWords = text.split(' ');
+    let numberOfCharacters = 0;
     
-    for (let i = 0; i<avarageWord.length;i++){
-      count+= avarageWord[i].length;
+    for (let i = 0; i<numberOfWords.length;i++){
+      numberOfCharacters+= numberOfWords[i].length;
     }
-    const result = (count/avarageWord.length).toFixed(2);
+    const result = (numberOfCharacters/numberOfWords.length).toFixed(2);
     return Number(result);
+
+    // O  método "split" "fatia" a string em array seguindo o regex,
+    // que determina que o breakpoint que separa um elemento é o espaço que separa antes e depois, essa array traz o números de palavras em 'numberOfWords'.
+    // O loop "for" define que enquanto o indice "i" for menor que o número de elementos da array de "numofNWords", soma-se esses elementos.
+    // E "numberOfCharacters" traz o comprimento dessa soma.
+    // const "result", faz a média do comprimento da soma das palavras de "numberOfCharacters"(que equivale ao total de caracteres) pelo número de palavras de 
+    // "numberofWords".
+    // O método "toFixed" permite definir a quantidade de casas decimais, o "2" indicado no parâmetro estabelece 2 casas decimais.
+    // O objeto "Number" permite converter o resultado dessa soma num valor de tipo 'number', que retorna na métrica exibida na tela da usuária.
+
+
+
+    // outra forma de resolver getAverageWordLength, mas não passa no teste porque tem que ter o "for":
+    // const numberOfWords = text.split (' ');
+    // const numberOfCharacters = numberOfWords.join('');
+    // return parseFloat((numberOfCharacters.length/numberOfWords.lengthnpm).toFixed(2));
   },
 };
  
-export default analyzer;
+export default analyzer; // Disponibiliza a const analyzer para ser importada em outro arquivo.
  
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import analyzer from './analyzer.js';
+import analyzer from './analyzer.js'; // importa a const analyzer do analyzer.js.
 
-const textarea=document.querySelector("textarea");
+const textarea=document.querySelector("textarea"); //pega os valores inseridos na textarea.
 
-const botao=document.getElementById("reset-button");
+const botao=document.getElementById("reset-button"); //chama o botão de limpar a textarea.
 
 function atualizaCaracter(){
 
@@ -18,11 +18,21 @@ function atualizaCaracter(){
 
   document.querySelector("li[data-testid='word-length-average']").textContent="Comprimento médio das palavras: "+ analyzer. getAverageWordLength(textarea.value) +" caracteres.";
     
+  // O "querySelector" é um seletor de DOM que retorna o primeiro elemento que corresponde ao seletor especificado (uma tag, uma class,
+  // ou até um name) de um seletor HTML.
+  // "textContent" é um atributo que faz a "manipulação dinâmica de DOM", trazendo o conteúdo de texto das li. Se coloca esse conteúdo
+  // entre " " para ele se manter exibido na tela da usuária, com a atualização dos caracteres.
+  // "analyzer.get..." invoca a função da const analyzer. Se não colocar o "(textarea.value)", retorna a função por extenso.
+
 }
-    
-textarea.addEventListener('input', atualizaCaracter);
+
+  
+textarea.addEventListener('input', atualizaCaracter); 
+// ao inserir valores na textarea as funções se atualizam e retornam na tela os contadores com as métricas atualizadas.
+
+
 botao.addEventListener('click', () =>{
   textarea.value='';
   atualizaCaracter();
-});
+});//ao clicar no botão a textarea é esvaziada e zera as funções que retornam as métricas na tela.
 
